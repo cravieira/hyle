3a7c152fda78ac3bed4b1fe4a06dc09feae671bd
Caio Vieira <cravieira@inf.ufrgs.br>
2024-09-24 13:48:12 -0300

    src: Add initial implementation of BSC
    Add a simple implementation of the BSC VSA class. This commit also adds
    a pretty printer that works on hls::vector.

--- src/hdc_tb.cpp
+++ src/hdc_tb.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <iostream>
+
+#include <ap_int.h>
+#include <hls_vector.h>
+
+constexpr int DIM = 100;
+using bin_t = ap_uint<1>;
+using hv_t = hls::vector<bin_t, DIM>;
+
+// Non-member printer function for hls::vector with binary digits
+std::ostream& operator<<(std::ostream& os, const hv_t v) {
+    for (auto it = v.cbegin(); it != v.cend(); it++) {
+        //os << std::hex << std::setw(1) << std::setfill('0') << *it;
+        os << *it;
+    }
+
+    return os;
+}
 
 int main () {
+    hv_t vec;
     printf("Hello World\n");
+    std::cout << vec << std::endl;
 }
+
 
