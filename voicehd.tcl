e47131ef8af241b979881ac515eebc8133bf41c0
Caio Vieira <cravieira@inf.ufrgs.br>
2024-10-21 13:30:18 -0300

    wip: Update voicehd.tcl to handle segments
    - Now it is possible to select the number of dimensions and segmenet
      size from the tcl file.
    - The HLS projects are named after the number of HV_DIM and
      HV_SEGMENT_SIZE.

--- voicehd.tcl
+++ voicehd.tcl
@@ -1,17 +1,28 @@
-open_project vitis_voicehd -reset
-add_file -tb "src/voicehd_tb.cpp"
-add_file -tb "src/common.hpp"
+# Define HV constants
+set DIM 10
+set DIM 1000
+set SEGMENT_SIZE 5
+set cflags "-D__HV_DIMENSIONS__=${DIM} -D__HV_SEGMENT_SIZE__=${SEGMENT_SIZE}"
 
-add_file "src/bsc.cpp"
-add_file "src/bsc.hpp"
-add_file "src/voicehd.cpp"
-add_file "src/voicehd.hpp"
+open_project "vitis_voicehd-d${DIM}-seg_size${SEGMENT_SIZE}" -reset
+#open_project vitis_voicehd
 
-set_top voicehd_enc
+add_files -cflags ${cflags} -tb "src/voicehd_tb.cpp"
+add_files -cflags ${cflags} -tb "src/common.hpp"
+add_files -cflags ${cflags} -tb "src/defines.hpp"
+
+add_files -cflags ${cflags} "src/bsc.cpp"
+add_files -cflags ${cflags} "src/bsc.hpp"
+add_files -cflags ${cflags} "src/voicehd.cpp"
+add_files -cflags ${cflags} "src/voicehd.hpp"
+
+set_top voicehd_enc_seg
 open_solution "solution1"
 set_part  {xc7z020clg400-1}
 create_clock -period 10
 csim_design
+#csim_design -O
 csynth_design
+#cosim_design -O
 
 export_design -flow syn
