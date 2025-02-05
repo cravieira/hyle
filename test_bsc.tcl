dc339947cfa6894abb9aec807f0db3db78cfa5b5
Caio Vieira <cravieira@inf.ufrgs.br>
2025-02-05 11:47:59 -0300

    Rename test files
    Rename BSC test files to avoid generic naming.

--- test_bsc.tcl
+++ test_bsc.tcl
@@ -1,16 +1,17 @@
-open_project vitis_main -reset
+# Run unit-tests on BSC operations.
+open_project vitis_test_bsc -reset
 
 # Define testbench constants
 set cflags "-D__HV_DIMENSIONS__=10 -D__HV_SEGMENT_SIZE__=10"
 
-add_files -tb -cflags ${cflags} "src/hdc_tb.cpp"
+add_files -tb -cflags ${cflags} "src/bsc_tb.cpp"
 add_files -tb -cflags ${cflags} "src/common.hpp"
 add_files -tb -cflags ${cflags} "src/defines.hpp"
 
 add_files -cflags ${cflags} "src/bsc.cpp"
 add_files -cflags ${cflags} "src/bsc.hpp"
 
-set_top bsc_bundle
+set_top bsc_bundle; # Set any top function
 open_solution "solution1"
 set_part  {xc7z020clg400-1}
 create_clock -period 10
