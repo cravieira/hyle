fdc5f9a16946b48008a839ce5f608fc9bf5f207d
Caio Vieira <cravieira@inf.ufrgs.br>
2025-02-28 11:12:01 -0300

    feat: Add more information to parse-results.py csv files
    Add initial interval (II), fmax, throughput (ops/sec), and efficiency
    (op/Area-CLB).

--- parse-results.py
+++ parse-results.py
@@ -22,6 +22,7 @@ def parse_project(path: str):
         csynth = json.load(f)
     d['top'] = csynth['Top']
     d['latency'] = csynth['ClockInfo']['Latency']
+    d['II'] = csynth['ClockInfo']['II']
     d['is_combinational'] = csynth['ClockInfo']['IsCombinational']
 
     # Implementation report
@@ -41,6 +42,31 @@ def parse_project(path: str):
 
     return d
 
+def add_extra_fields(d: dict):
+    """
+    Add extra information to a parsed report dictionary.
+    """
+    cp = d['achieved_cp']
+    initial_interval = int(d['II'])
+
+    fmax_str = 'NA'
+    throughput_str = 'NA'
+    efficiency_str = 'NA'
+    if cp != 'NA':
+        fmax = 1 / (float(cp) * 10**-9) # Clock period is provided in nano seconds
+        throughput = fmax/initial_interval
+        efficiency = throughput/float(d['clb'])
+
+        fmax_str = f'{fmax:.3f}'
+        throughput_str = f'{throughput:.3f}'
+        efficiency_str = f'{efficiency:.3f}'
+
+    d['fmax'] = fmax_str
+    d['throughput'] = throughput_str
+    d['efficiency'] = efficiency_str
+
+    return d
+
 def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument(
@@ -54,11 +80,12 @@ def main():
     )
     args = parser.parse_args()
 
-    rpts = list(map(parse_project, args.vitis_projects))
+    rpts_raw = list(map(parse_project, args.vitis_projects))
+    rpts = list(map(add_extra_fields, rpts_raw))
 
     # Write CSV
     with open(args.csv_file, 'w', newline='') as csvfile:
-        fieldnames = ['name', 'top', 'latency', 'is_combinational', 'achieved_cp', 'target_cp', 'cp_met', 'bram', 'clb', 'dsp', 'ff', 'latch', 'lut']
+        fieldnames = ['name', 'top', 'latency', 'is_combinational', 'achieved_cp', 'target_cp', 'cp_met', 'bram', 'clb', 'dsp', 'ff', 'latch', 'lut', 'fmax', 'II', 'throughput', 'efficiency']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
