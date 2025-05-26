9684aa6cd41cc0812635f1c9ea41e44455509d95
Caio Vieira <cravieira@inf.ufrgs.br>
2025-05-26 13:51:09 -0300

    feat: Add voicehd DSE
    - Extend hdchog DSE to also include the DSE of voicehd.
    - Rename dse_hdchog.sh to dse.sh.

--- dse.sh
+++ dse.sh
@@ -15,8 +15,9 @@ DATAPATHS=""
 SEGMENT_SIZE=""
 CLOCK_PERIODS="5 10 15"
 
-MAIN_TCL="hdchog.tcl"
-PARAM_TEMP_DIR="_tmp_hdchog"
+APP_NAME="" # Name of app to be executed
+MAIN_TCL="" # Name of tcl script
+PARAM_TEMP_DIR="_tmp_dse" # Temporary dir for parameter files
 
 proj_name() {
     local vsa=$1
@@ -31,7 +32,7 @@ proj_name() {
         model_name="$vsa_class$cgr_points"
     fi
 
-    echo "vitis_dse_hdchog-${model_name}-d${dim}-seg_size${ss}-dp${dp}-cp${cp}"
+    echo "vitis_dse_${APP_NAME}-${model_name}-d${dim}-seg_size${ss}-dp${dp}-cp${cp}"
 }
 
 dse() {
@@ -52,28 +53,50 @@ dse() {
     echo "$cmd"
 }
 
-cmd=""
-# CGR
-VSAS="cgr-4"
-DIMS="2048"
-SEGMENT_SIZE=256
-DATAPATHS="1 2 4 8"
-cmd+=$(dse)
+cgr_dse() {
+    local dse_cmds=""
+    # CGR
+    VSAS="cgr-4"
+    DIMS="2048"
+    SEGMENT_SIZE=256
+    DATAPATHS="1 2 4 8"
+    dse_cmds+=$(dse)
 
-# Narrower CGR datapaths
-DATAPATHS="1"
-SEGMENT_SIZE=128
-cmd+=$(dse)
-SEGMENT_SIZE=64 # This design is too small compared to the BSC designs evaluated
-cmd+=$(dse)
+    # Narrower CGR datapaths
+    DATAPATHS="1"
+    SEGMENT_SIZE=128
+    dse_cmds+=$(dse)
+    SEGMENT_SIZE=64 # This design is too small compared to the BSC designs evaluated
+    dse_cmds+=$(dse)
 
-# BSC
-VSAS="bsc"
-DIMS="8192 16384 32768"
-DIMS="16384 32768"
-SEGMENT_SIZE=256
-DATAPATHS="1 2 4 8 16"
-#DATAPATHS="32" # Failing in Vivado implementation due to high number of IOs in the IP
-cmd+=$(dse)
+    echo "$dse_cmds"
+}
+
+bsc_dse() {
+    local dse_cmds=""
+
+    # BSC
+    VSAS="bsc"
+    DIMS="8192 16384 32768"
+    DIMS="16384 32768"
+    SEGMENT_SIZE=256
+    DATAPATHS="1 2 4 8 16"
+    #DATAPATHS="32" # Failing in Vivado implementation due to high number of IOs in the IP
+    dse_cmds+=$(dse)
+
+    echo "$dse_cmds"
+}
+
+APP_NAME="hdchog"
+MAIN_TCL="${APP_NAME}.tcl"
+cmd+=""
+cmd+=$(cgr_dse)
+cmd+=$(bsc_dse)
+
+APP_NAME="voicehd"
+MAIN_TCL="${APP_NAME}.tcl"
+cmd+=""
+cmd+=$(cgr_dse)
+cmd+=$(bsc_dse)
 
 printf "$cmd"
